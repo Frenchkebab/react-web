2faa7ecbaaf2a12f69288c92bc39bac9196e535f
frenchkebab <tubowangin@gmail.com>
2022-05-22 19:52:38 +0900

    [#6] using useEffect, you can run a callback function only once when it gets rendered for first time

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('call an api');
+  console.log('I run all the time');
+  const iRunOnlyOnce = () => {
+    console.log('I run only once');
+  };
+  useEffect(() => {
+    console.log('call the api...');
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
