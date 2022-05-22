e13068b2a82344a533f19768bbab219994cdd491
frenchkebab <tubowangin@gmail.com>
2022-05-22 19:47:58 +0900

    [#6] The whole component gets re-rendered each time the state changes - how do we make it run only once at the very first time it gets rendered?

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,13 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('call an api');
   return (
-    <div className={styles.title}>
-      <h1>Welcome back!</h1>
-      <Button text="Continue" />
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
