66a40c91b0762ca37b8a6d86092a8c8a4570f18f
frenchkebab <tubowangin@gmail.com>
2022-05-25 16:52:46 +0900

    [#8] Challenge - calculate how much you can buy

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,6 +3,21 @@ import React, { useState, useEffect } from 'react';
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState('0');
+
+  const inputHandler = (event) => setAmount(event.target.value);
+
+  const onSelect = (event) => {
+    for (let coin of coins) {
+      if (coin.symbol === event.target.value) {
+        setSelectedCoin(coin);
+        console.log(coin);
+        return;
+      }
+    }
+  };
+
   useEffect(() => {
     fetch('https://api.coinpaprika.com/v1/tickers')
       .then((response) => response.json())
@@ -11,10 +26,45 @@ const App = () => {
         setLoading(false);
       });
   }, []);
+
   return (
     <div>
       <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
+
+      <form>
+        <div>
+          {loading ? (
+            <strong>Loading...</strong>
+          ) : (
+            <div>
+              <div>
+                <input
+                  value={amount}
+                  placeholder="$ you have"
+                  type="number"
+                  onChange={inputHandler}
+                />
+              </div>
+
+              <select onChange={onSelect}>
+                <option value="0">Please Select</option>
+                {coins.map((coin, idx) => (
+                  <option value={coin.symbol} key={idx}>
+                    {coin.name} ({coin.symbol}) : (${coin.quotes.USD.price.toFixed(6)})
+                  </option>
+                ))}
+              </select>
+              <span>
+                {amount && selectedCoin ? (amount / selectedCoin.quotes.USD.price).toFixed(4) : 0}
+              </span>
+            </div>
+          )}
+        </div>
+      </form>
+
+      <br />
+      <hr />
+      <br />
       <ul>
         {coins.map((coin, idx) => (
           <li key={idx}>
