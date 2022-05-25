b11634ac63b657f5393b59731867ac9a4749fb83
frenchkebab <tubowangin@gmail.com>
2022-05-25 23:29:08 +0900

    [#9] Challenge - Detail Page

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,21 +1,45 @@
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
 
 const Detail = () => {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
   const { id } = useParams();
 
   const getMovie = async () => {
     const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    console.log(json.data.movie);
+    setLoading(false);
+    setMovie(json.data.movie);
   };
 
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <Link to={`/`}>
+        <h1>Home</h1>
+      </Link>
+
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          coverImg={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.summary}
+          genres={movie.genres}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Detail;
