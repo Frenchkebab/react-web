9db8f0617461827ff0c8d78e47cdb71e1d968646
frenchkebab <tubowangin@gmail.com>
2022-05-25 19:41:07 +0900

    [#9] fetching movie data and store it in 'movies' state - using promise

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,79 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState('0');
-
-  const inputHandler = (event) => setAmount(event.target.value);
-
-  const onSelect = (event) => {
-    for (let coin of coins) {
-      if (coin.symbol === event.target.value) {
-        setSelectedCoin(coin);
-        console.log(coin);
-        return;
-      }
-    }
-  };
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    ).then((response) =>
+      response.json().then((json) => {
+        setMovies(json.data.movies);
         setLoading(false);
-      });
+      })
+    );
   }, []);
-
-  return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-
-      <form>
-        <div>
-          {loading ? (
-            <strong>Loading...</strong>
-          ) : (
-            <div>
-              <div>
-                <input
-                  value={amount}
-                  placeholder="$ you have"
-                  type="number"
-                  onChange={inputHandler}
-                />
-              </div>
-
-              <select onChange={onSelect}>
-                <option value="0">Please Select</option>
-                {coins.map((coin, idx) => (
-                  <option value={coin.symbol} key={idx}>
-                    {coin.name} ({coin.symbol}) : (${coin.quotes.USD.price.toFixed(6)})
-                  </option>
-                ))}
-              </select>
-              <span>
-                {amount && selectedCoin ? (amount / selectedCoin.quotes.USD.price).toFixed(4) : 0}
-              </span>
-            </div>
-          )}
-        </div>
-      </form>
-
-      <br />
-      <hr />
-      <br />
-      <ul>
-        {coins.map((coin, idx) => (
-          <li key={idx}>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 };
 
 export default App;
