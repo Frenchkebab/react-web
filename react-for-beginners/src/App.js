69f0fe175e95dc066a409b141aa6ed2257e30edc
frenchkebab <tubowangin@gmail.com>
2022-05-25 21:25:45 +0900

    [#9] About Router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,46 +1,8 @@
 import { useState, useEffect } from 'react';
-import Movie from './Movie';
+import Movie from './components/Movie';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          <ul>
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                coverImg={movie.medium_cover_image}
-                title={movie.title}
-                summary={movie.summary}
-                genres={movie.genres}
-              />
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+  return null;
 };
 
 export default App;
